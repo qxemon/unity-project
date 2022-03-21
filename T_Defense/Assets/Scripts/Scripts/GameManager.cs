8658ac2c1bbba9af9fdfa943321fdffa95da6a81
ChoHyeonSeok <hs.cho9647@gmail.com>
2022-03-21 20:10:43 +0900

    0321진행
    0321진행
    상태창 패널, 시작버튼

--- T_Defense/Assets/Scripts/Scripts/GameManager.cs
+++ T_Defense/Assets/Scripts/Scripts/GameManager.cs
@@ -9,11 +9,13 @@ public class GameManager : MonoBehaviour
     public static GameManager gm;
     private GameObject[] obj;
 
+    #region 각종 필요 속성 선언
     public int myGold = 0;
     public GameObject TowerPrefab;
-
+    public GameObject RoundButton;
     public int PlayRound;
     public int nowRound = 0;
+    #endregion
     // UI Text Variable
     Text UItext;
     private void Awake()
@@ -40,6 +42,8 @@ public class GameManager : MonoBehaviour
         gState = GameState.Play;
         Debug.Log("Starting");
         this.veiling();
+        int Hp = GameObject.Find("Castle").GetComponent<TowerHP>().CastleHp;
+        GameObject.Find("hpOfCastle").GetComponent<Text>().text = "Castle HP : " + Hp.ToString();
         // 게임종료상태 설정
 
     }
@@ -47,10 +51,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int Hp = GameObject.Find("Castle").GetComponent<TowerHP>().CastleHp;
-        GameObject.Find("hpOfCastle").GetComponent<Text>().text = "Castle HP : " + Hp.ToString();
-        
+        //int Hp = GameObject.Find("Castle").GetComponent<TowerHP>().CastleHp;
+        //GameObject.Find("hpOfCastle").GetComponent<Text>().text = "Castle HP : " + Hp.ToString();
     }
+    #region 각종 기능 함수
+
+    #region target 초기화
     private void veiling()
     {
         obj = GameObject.FindGameObjectsWithTag("Target");
@@ -60,6 +66,9 @@ public class GameManager : MonoBehaviour
             string message = i.ToString();
         }
     }
+    #endregion
+
+    #region 골드 획득 함수
     // 골드 획득량 증가 함수
     public void GetGold(GameObject obj) {
         myGold++;
@@ -67,16 +76,24 @@ public class GameManager : MonoBehaviour
         Debug.Log("Gold : " + myGold.ToString());
         obj.GetComponent<Coin>().Collect();
     }
+    #endregion
+
+    #region 웨이브 시작함수
     // 웨이브 시작함수
     public void WaveStart(GameObject obj) {
         Debug.Log("TEST : ");
         // Data Chaning by wave
         nowRound++;
         this.NowRound();
-        float[] data = new float[3]{1, 2, 5};
+        // data = {라운드 시작되는 term, 소환 interval, 반복횟수}
+        // {x, y, z} => x초 후에 y초 간격으로 z번 반복해라
+        float[] data = new float[3]{1, 2, nowRound * 5};
         GameObject.Find("Gate").SendMessage("WaveStart", data);
         Destroy(obj);
     }
+    #endregion
+
+    #region 타워생성
     // 타워 생성
     public void CreateTower(GameObject obj) {
         if (myGold >= 5) {
@@ -88,7 +105,16 @@ public class GameManager : MonoBehaviour
             Debug.Log("Message : not enough Gold");
         }
     }
+    #endregion
+    
+    #region 시작버튼 생성
+    // 시작버튼 생성
+    public void WaveStartButton() {
+        GameObject.Find("Gate").GetComponent<WaveSpawn>().SendMessage("CreateButton");
+    }
+    #endregion
 
+    #region 상태창 텍스트 
     // Round 텍스트 변경
     public void NowRound() {
         string s = "현재 라운드 : " + nowRound.ToString() + "/" + "임시 총라운드";
@@ -98,4 +124,10 @@ public class GameManager : MonoBehaviour
         string s = "Gold : " + myGold.ToString();
         GameObject.Find("Gold").GetComponent<Text>().text = s;
     }
+    public void HP(int hp) {
+        GameObject.Find("hpOfCastle").GetComponent<Text>().text = "Castle HP : " + hp.ToString();
+    }
+    #endregion
+
+    #endregion
 }
--- T_Defense/Assets/Scripts/Scripts/Objects/WaveSpawn.cs
+++ T_Defense/Assets/Scripts/Scripts/Objects/WaveSpawn.cs
@@ -3,17 +3,26 @@ using System.Collections;
 public class WaveSpawn : MonoBehaviour {
 
 	public int WaveSize;
+	public int totalWave;
 	public GameObject EnemyPrefab;
+	public GameObject StartButton;
 	public Transform spawnPoint;
 	public Transform[] WayPoints;
 	int enemyCount=0;
+	int wavecount = 0;
 
 	void Update()
 	{
 		if(enemyCount == WaveSize)
 		{
 			CancelInvoke("SpawnEnemy");
+			enemyCount = 0;
+			if (totalWave > wavecount)
+			{
+				CreateButton();
+			}
 		}
+		
 	}
 
 	void SpawnEnemy()
@@ -29,5 +38,23 @@ public class WaveSpawn : MonoBehaviour {
 		InvokeRepeating("SpawnEnemy", data[0], data[1]);
 		WaveSize = (int) data[2];
 		enemyCount = 0;
+		wavecount++;
+	}
+
+	void CreateButton() {
+		// Transform tp = new Vector3(spawnPoint.transform.position.x, 
+		// 						   spawnPoint.transform.position.y, 
+		// 						   spawnPoint.transform.position.z + 10);
+		// tp.position.z += 10;
+
+		// Transform tempP = spawnPoint;
+		// tempP.localPosition = new Vector3(spawnPoint.transform.position.x, 
+		// 						   spawnPoint.transform.position.y, 
+		// 						   spawnPoint.transform.position.z + 10);
+		// Instantiate(StartButton, tempP.position, this.transform.rotation);
+
+
+		Instantiate(StartButton, new Vector3((float)2.7, (float)4.5, (float)-2.65), this.transform.rotation);
+
 	}
 }
